7f35cd44d6b9a929769b016c0169eabaf583aa92
icorreia <icorreia@student.dei.uc.pt>
2011-06-09 00:38:01 +0100

    Nao sei se ja fiz push ou nao :P

--- v4/gcdRrec.c
+++ v4/gcdRrec.c
@@ -23,21 +23,27 @@ int temp2 = 1;
 if (temp1 != temp2) goto NEXTLABEL1;
 char temp3[1025]; temp3[0] = '\0';strcat(temp3, "First");
 printf("%s\n", temp3);
+char temp4[1025]; temp4[0] = '\0';strcat(temp4, "First");
+printf("%s\n", temp4);
 goto ENDCYCLE0;
 NEXTLABEL1: ;
-int temp4 = 2;
-if (temp1 != temp4) goto NEXTLABEL2;
-char temp5[1025]; temp5[0] = '\0';strcat(temp5, "Second");
-printf("%s\n", temp5);
+int temp5 = 2;
+if (temp1 != temp5) goto NEXTLABEL2;
+char temp6[1025]; temp6[0] = '\0';strcat(temp6, "Second");
+printf("%s\n", temp6);
+char temp7[1025]; temp7[0] = '\0';strcat(temp7, "Second");
+printf("%s\n", temp7);
 goto ENDCYCLE0;
 NEXTLABEL2: ;
-char temp6[1025]; temp6[0] = '\0';strcat(temp6, "Third");
-printf("%s\n", temp6);
+char temp8[1025]; temp8[0] = '\0';strcat(temp8, "Third");
+printf("%s\n", temp8);
+char temp9[1025]; temp9[0] = '\0';strcat(temp9, "Third");
+printf("%s\n", temp9);
 goto ENDCYCLE0;
 NEXTLABEL3: ; ENDCYCLE0: ;
-char temp7[1025]; temp7[0] = '\0';strcat(temp7, "End");
-printf("%s\n", temp7);
-int temp8 = 0;
+char temp10[1025]; temp10[0] = '\0';strcat(temp10, "End");
+printf("%s\n", temp10);
+int temp11 = 0;
 goto EPILOGUE_main;
 
 /*METHOD: recursive */
@@ -52,9 +58,9 @@ sp->locals[0] = (int*) malloc(sizeof(int));
 (*((int*) sp->locals[0])) = (*((int*) sp->parent->outgoing[0]));
 
 /*Method's body.*/
-double temp9 = 2.000000;
+double temp12 = 2.000000;
 sp->parent->returnValue = (double*) malloc(sizeof(double));
-(*((double*) sp->parent->returnValue)) = temp9;
+(*((double*) sp->parent->returnValue)) = temp12;
 goto EPILOGUE_recursive;
 
 /*Epilogue*/
